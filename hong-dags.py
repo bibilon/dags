80ec4a65b7251e04ec7981673aa8017c5593934b
hongtt0102 <146858181+hongtt0102@users.noreply.github.com>
2024-05-18 17:51:25 +0700

    Update hong-dags.py

--- hong-dags.py
+++ hong-dags.py
@@ -43,5 +43,5 @@ with DAG(
     bash_command="kubectl get sparkapplication hongtt-spark-job-18 -n spark-jobs -o=jsonpath={.status.applicationState.state}",
     xcom_push=True,
     dag=dag
-)
+    )
    start >> t1 >> get_spark_status
