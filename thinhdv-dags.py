f7cd4bc2563d9936c0d8202f9773cfa7e429049d
Doan Van Thinh <49871433+thinhhoainam@users.noreply.github.com>
2024-05-20 15:50:24 +0700

    Update thinhdv-dags.py

--- thinhdv-dags.py
+++ thinhdv-dags.py
@@ -20,36 +20,82 @@ with DAG(
    default_args=default_args,
    description='simple dag',
    schedule_interval=timedelta(days=1),
-   start_date=datetime(2024, 5, 16),
+   start_date=datetime(2024, 5, 20),
    catchup=False,
    tags=['example13'],
    template_searchpath='/opt/airflow/dags/repo/'
 ) as dag:
    start = EmptyOperator(task_id="start")
    t1 = SparkKubernetesOperator(
-       task_id='n-spark',
-       trigger_rule="all_success",
-       depends_on_past=False,
+       task_id='load_RP_SUB_PRE'
        retries=0,
        namespace='spark-jobs',
-       application_file="hongtt_sparkjob.yaml",
+       application_file="rp_sub_pre.yaml",
        kubernetes_conn_id="myk8s",
        do_xcom_push=True,
        dag=dag
    )
-   spark_sensor = SparkKubernetesSensor(
+   spark_sensor_1 = SparkKubernetesSensor(
     task_id='spark_sensor',
     namespace='spark-jobs',
-    application_name='hongtt-spark-job-18',
+    application_name='spark-load-rp-sub-pre',
     kubernetes_conn_id='myk8s',
     dag=dag
     )
-   delete_task = KubernetesPodOperator(
+   delete_task_1 = KubernetesPodOperator(
     task_id='delete_spark_application',
     namespace='spark-jobs',
     image='bitnami/kubectl:latest',
-    cmds=['kubectl', 'delete', 'sparkapplication', 'hongtt-spark-job-18', '-n', 'spark-jobs'],
+    cmds=['kubectl', 'delete', 'sparkapplication', 'spark-load-rp-sub-pre', '-n', 'spark-jobs'],
     get_logs=True,
     dag=dag
     )
-   start >> t1 >> spark_sensor >> delete_task
+   t2 = SparkKubernetesOperator(
+       task_id='load_COMM_RP'
+       retries=0,
+       namespace='spark-jobs',
+       application_file="comm_rp.yaml",
+       kubernetes_conn_id="myk8s",
+       do_xcom_push=True,
+       dag=dag
+   )
+   spark_sensor_2 = SparkKubernetesSensor(
+    task_id='spark_sensor',
+    namespace='spark-jobs',
+    application_name='spark-comm-rp',
+    kubernetes_conn_id='myk8s',
+    dag=dag
+    )
+   delete_task_2 = KubernetesPodOperator(
+    task_id='delete_spark_application',
+    namespace='spark-jobs',
+    image='bitnami/kubectl:latest',
+    cmds=['kubectl', 'delete', 'sparkapplication', 'spark-comm-rp', '-n', 'spark-jobs'],
+    get_logs=True,
+    dag=dag
+    )
+   t3 = SparkKubernetesOperator(
+       task_id='update_phi'
+       retries=0,
+       namespace='spark-jobs',
+       application_file="pbh.yaml",
+       kubernetes_conn_id="myk8s",
+       do_xcom_push=True,
+       dag=dag
+   )
+   spark_sensor_3 = SparkKubernetesSensor(
+    task_id='spark_sensor',
+    namespace='spark-jobs',
+    application_name='spark-update-phi',
+    kubernetes_conn_id='myk8s',
+    dag=dag
+    )
+   delete_task_3 = KubernetesPodOperator(
+    task_id='delete_spark_application',
+    namespace='spark-jobs',
+    image='bitnami/kubectl:latest',
+    cmds=['kubectl', 'delete', 'sparkapplication', 'spark-update-phi', '-n', 'spark-jobs'],
+    get_logs=True,
+    dag=dag
+    )
+   start >> t1 >> spark_sensor_1 >> delete_task_1 >> t2 >> spark_sensor_2 >> delete_task_2 >> t3 >> spark_sensor_3 >> delete_task_3  
