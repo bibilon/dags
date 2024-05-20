32d84ec8d8fa0593b78b913c8d9ad39fafe1c130
Doan Van Thinh <49871433+thinhhoainam@users.noreply.github.com>
2024-05-20 17:10:52 +0700

    Update test-dags.py

--- test-dags.py
+++ test-dags.py
@@ -138,12 +138,21 @@ with DAG(
        dag=dag
    )
 
+   branch_task_3 = BranchPythonOperator(
+       task_id='branch_task_2',
+       python_callable=decide_which_path,
+       provide_context=True,
+       op_kwargs={'upstream_task_id': 'previous_task_id'},
+       dag=dag
+   )
+
    handle_error_task = DummyOperator(
        task_id='handle_error',
        dag=dag
    )
 
-   start >> t1 >> spark_sensor_1 >> branch_task >> delete_task_1 >> t2 >> spark_sensor_2 >> branch_task_2 >> delete_task_2 >> end
+   start >> t1 >> spark_sensor_1 >> branch_task >> delete_task_1 >> t2 >> spark_sensor_2 >> branch_task_2 >> delete_task_2 >> t3 >> spark_sensor_3 >> branch_task_3 >> delete_task_3 >> end
    branch_task >> handle_error_task
    branch_task_2 >> handle_error_task
+   branch_task_3 >> handle_error_task
    handle_error_task >> end
