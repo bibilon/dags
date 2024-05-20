430e0f217b4dd496d25f9a2d0bd7565cc78dd401
Doan Van Thinh <49871433+thinhhoainam@users.noreply.github.com>
2024-05-20 16:23:31 +0700

    Update test-dags.py

--- test-dags.py
+++ test-dags.py
@@ -1,7 +1,117 @@
-from airflow.operators.python import BranchPythonOperator
+from airflow import DAG
+from datetime import timedelta, datetime
+from airflow.providers.cncf.kubernetes.operators.spark_kubernetes import SparkKubernetesOperator
+from airflow.providers.cncf.kubernetes.sensors.spark_kubernetes import SparkKubernetesSensor
+from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import BranchPythonOperator
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 3, 12),
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1
+}
+
+dag = DAG(
+    'thinhdv-dags',
+    default_args=default_args,
+    description='simple dag',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2024, 5, 20),
+    catchup=False,
+    tags=['example13'],
+    template_searchpath='/opt/airflow/dags/repo/'
+)
+
+# Define tasks
+start = DummyOperator(task_id="start", dag=dag)
+
+t1 = SparkKubernetesOperator(
+    task_id='load_RP_SUB_PRE',
+    retries=0,
+    namespace='spark-jobs',
+    application_file="rp_sub_pre.yaml",
+    kubernetes_conn_id="myk8s",
+    do_xcom_push=True,
+    dag=dag
+)
+
+spark_sensor_1 = SparkKubernetesSensor(
+    task_id='spark_sensor_spark_load_rp_sub_pre',
+    namespace='spark-jobs',
+    application_name='spark-load-rp-sub-pre',
+    kubernetes_conn_id='myk8s',
+    dag=dag
+)
+
+delete_task_1 = KubernetesPodOperator(
+    task_id='delete_spark_application_load_rp_sub_pre',
+    namespace='spark-jobs',
+    image='bitnami/kubectl:latest',
+    cmds=['kubectl', 'delete', 'sparkapplication', 'spark-load-rp-sub-pre', '-n', 'spark-jobs'],
+    get_logs=True,
+    dag=dag
+)
+
+t2 = SparkKubernetesOperator(
+    task_id='load_COMM_RP',
+    retries=0,
+    namespace='spark-jobs',
+    application_file="comm_rp.yaml",
+    kubernetes_conn_id="myk8s",
+    do_xcom_push=True,
+    dag=dag
+)
+
+spark_sensor_2 = SparkKubernetesSensor(
+    task_id='spark_sensor_load_COMM_RP',
+    namespace='spark-jobs',
+    application_name='spark-comm-rp',
+    kubernetes_conn_id='myk8s',
+    dag=dag
+)
+
+delete_task_2 = KubernetesPodOperator(
+    task_id='delete_spark_application_load_COMM_RP',
+    namespace='spark-jobs',
+    image='bitnami/kubectl:latest',
+    cmds=['kubectl', 'delete', 'sparkapplication', 'spark-comm-rp', '-n', 'spark-jobs'],
+    get_logs=True,
+    dag=dag
+)
+
+t3 = SparkKubernetesOperator(
+    task_id='update_phi',
+    retries=0,
+    namespace='spark-jobs',
+    application_file="pbh.yaml",
+    kubernetes_conn_id="myk8s",
+    do_xcom_push=True,
+    dag=dag
+)
+
+spark_sensor_3 = SparkKubernetesSensor(
+    task_id='spark_sensor_update_phi',
+    namespace='spark-jobs',
+    application_name='spark-update-phi',
+    kubernetes_conn_id='myk8s',
+    dag=dag
+)
+
+delete_task_3 = KubernetesPodOperator(
+    task_id='delete_spark_application_update_phi',
+    namespace='spark-jobs',
+    image='bitnami/kubectl:latest',
+    cmds=['kubectl', 'delete', 'sparkapplication', 'spark-update-phi', '-n', 'spark-jobs'],
+    get_logs=True,
+    dag=dag
+)
+
+# Define branching logic
 def decide_branch(**kwargs):
     task_instance = kwargs['ti']
     task_id = task_instance.task_id
@@ -13,13 +123,14 @@ def decide_branch(**kwargs):
 branching = BranchPythonOperator(
     task_id='branching',
     python_callable=decide_branch,
-    provide_context=True
+    provide_context=True,
+    dag=dag
 )
 
 # Define DummyOperators for each task to handle different branches
-dummy_fail_t1 = DummyOperator(task_id='t1_failed')
-dummy_fail_t2 = DummyOperator(task_id='t2_failed')
-dummy_fail_t3 = DummyOperator(task_id='t3_failed')
+dummy_fail_t1 = DummyOperator(task_id='t1_failed', dag=dag)
+dummy_fail_t2 = DummyOperator(task_id='t2_failed', dag=dag)
+dummy_fail_t3 = DummyOperator(task_id='t3_failed', dag=dag)
 
 # Define TriggerDagRunOperator for each task to rerun the DAG with different branches
 trigger_fail_t1 = TriggerDagRunOperator(
@@ -43,12 +154,11 @@ trigger_fail_t3 = TriggerDagRunOperator(
     dag=dag
 )
 
-start >> t1 >> branching
-start >> t2 >> branching
-start >> t3 >> branching
+# Define dependencies
+start >> t1 >> spark_sensor_1 >> delete_task_1 >> branching
+start >> t2 >> spark_sensor_2 >> delete_task_2 >> branching
+start >> t3 >> spark_sensor_3 >> delete_task_3 >> branching
 
-# Define the branching logic
 branching >> [dummy_fail_t1, dummy_fail_t2, dummy_fail_t3]
 
-# Define the failure paths
 [dummy_fail_t1, dummy_fail_t2, dummy_fail_t3] >> [trigger_fail_t1, trigger_fail_t2, trigger_fail_t3]
