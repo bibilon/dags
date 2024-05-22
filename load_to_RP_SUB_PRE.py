ed7cd74be37bbfa09bdc4ae348758ae186f6bbc6
Doan Van Thinh <49871433+thinhhoainam@users.noreply.github.com>
2024-05-22 13:43:04 +0700

    Update load_to_RP_SUB_PRE.py

--- load_to_RP_SUB_PRE.py
+++ load_to_RP_SUB_PRE.py
@@ -36,7 +36,7 @@ with DAG(
    'load_RP_SUB_PRE_dags',
    default_args=default_args,
    description='simple dag',
-   schedule_interval=timedelta(days=1),
+   schedule_interval='50 13 * * *',
    start_date=datetime(2024, 5, 20),
    catchup=False,
    tags=['example13'],
