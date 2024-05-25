aec934683cabafbd7a2abaf1661876d3eb0ad8e6
hongtt0102 <146858181+hongtt0102@users.noreply.github.com>
2024-05-25 16:48:12 +0700

    Update hongtt_test_trigger.py

--- hongtt_test_trigger.py
+++ hongtt_test_trigger.py
@@ -6,6 +6,7 @@ from requests.auth import HTTPBasicAuth
 from airflow.operators.empty import EmptyOperator
 from airflow.sensors.http_sensor import HttpSensor
 from airflow.hooks.http_hook import HttpHook
+from airflow.exceptions import AirflowException
 # Hàm trigger notebook trong Zeppelin
 def trigger_notebook():
     url = "http://192.168.121.112:31818/api/notebook/job/2JX2D44RY"
