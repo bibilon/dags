9a62372390a54f979b2c9ca12baf0180ac7909d3
Doan Van Thinh <49871433+thinhhoainam@users.noreply.github.com>
2024-05-27 08:44:46 +0700

    Update ZEPPELIN_load_to_RP_SUB_PRE.py

--- ZEPPELIN_load_to_RP_SUB_PRE.py
+++ ZEPPELIN_load_to_RP_SUB_PRE.py
@@ -21,7 +21,7 @@ with DAG(
    'ZEPPELIN_load_to_RP_SUB_PRE',
    default_args=default_args,
    description='simple dag',
-   schedule_interval='18 15 * * *',
+   schedule_interval='50 1 * * *',
    start_date=datetime(2024, 5, 16),
    catchup=False,
    tags=['example13'],
