de4c56ee168465e2584598be295bfb4ec4cc6336
hongtt0102 <146858181+hongtt0102@users.noreply.github.com>
2024-11-07 09:31:20 +0700

    Update testdagjdbc.py

--- testdagjdbc.py
+++ testdagjdbc.py
@@ -3,7 +3,7 @@ from airflow.operators.dummy import DummyOperator
 from airflow.models import Variable
 from datetime import datetime
 import cx_Oracle
-
+from airflow.utils.task_group import TaskGroup
 # Hàm kết nối đến cơ sở dữ liệu Oracle và lấy tên DAG
 def get_dynamic_dag_names():
     # Lấy các thông tin kết nối từ Airflow Variables
@@ -21,19 +21,19 @@ def get_dynamic_dag_names():
     
     try:
         cursor = connection.cursor()
-        cursor.execute("SELECT dag_name, schedule_interval FROM DAG_NAMES")  # Thay đổi câu lệnh truy vấn nếu cần
+        cursor.execute("SELECT dag_name, schedule_interval, notebookid FROM DAG_NAMES")  # Thay đổi câu lệnh truy vấn nếu cần
         dag_configs = cursor.fetchall()
     finally:
         cursor.close()
         connection.close()
     
-    return [(config[0], config[1]) for config in dag_configs]  # Trả về danh sách cấu hình DAG
+    return [(config[0], config[1], config[2]) for config in dag_configs]  # Trả về danh sách cấu hình DAG
 
 # Lấy cấu hình DAG từ cơ sở dữ liệu
 dag_configs = get_dynamic_dag_names()
 
 # Tạo DAG cho mỗi cấu hình lấy được
-for dag_name, schedule_interval in dag_configs:
+for dag_name, schedule_interval, notebookid in dag_configs:
     default_args = {
         'owner': 'airflow',
         'start_date': datetime(2023, 1, 1),  # Đặt start_date theo yêu cầu của bạn
@@ -49,7 +49,22 @@ for dag_name, schedule_interval in dag_configs:
 
     # Thêm một DummyOperator làm nhiệm vụ mẫu
     with dag:
-        start = DummyOperator(task_id='start')
+        trigger_notebook_task = PythonOperator(
+            task_id='trigger_notebook',
+            python_callable=trigger_notebook,
+            op_kwargs={'nodepadID': f'{notebookid}'},
+            dag=dag
+        )
+       sensor_task =  CustomHttpSensor(
+            task_id='check_status_notebook',
+            method='GET',
+            http_conn_id='zeppelin_http_conn',  # Định nghĩa kết nối HTTP trong Airflow
+            endpoint=f'/api/notebook/job/{notebookid}',  # Thay {note_id} bằng ID của notebook Zeppelin
+            headers={"Content-Type": "application/json"},
+            timeout=120,  # Thời gian chờ tối đa
+            poke_interval=60,  # Khoảng thời gian giữa các lần kiểm tra
+            dag=dag,
+        )
 
     # Đăng ký DAG vào globals để Airflow có thể nhận diện
     globals()[dag_name] = dag
