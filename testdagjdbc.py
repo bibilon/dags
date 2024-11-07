50944b61f43bf63046ebc69a03869dd2021bb746
hongtt0102 <146858181+hongtt0102@users.noreply.github.com>
2024-11-07 10:18:36 +0700

    Update testdagjdbc.py

--- testdagjdbc.py
+++ testdagjdbc.py
@@ -74,13 +74,13 @@ for dag_name, schedule_interval, notebookid in dag_configs:
             poke_interval=60,  # Khoảng thời gian giữa các lần kiểm tra
             dag=dag,
         )
-        restart_interputer = PythonOperator(
-            task_id='restart_notebook',
-            python_callable=restart_notebook,
+        restart_interpreter_notebook = PythonOperator(
+            task_id='restart_interpreter_notebook',
+            python_callable=restart_interpreter_notebook,
             op_kwargs={'nodepadID': f'{notebookid}'},
             dag=dag
         )
-        trigger_notebook_task >> sensor_task >> restart_interputer
+        trigger_notebook_task >> sensor_task >> restart_interpreter_notebook
 
     # Đăng ký DAG vào globals để Airflow có thể nhận diện
     globals()[dag_name] = dag
