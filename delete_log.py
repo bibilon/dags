bdded2fac3cfae09e20536f869296ff4658c8acb
hongtt0102 <146858181+hongtt0102@users.noreply.github.com>
2025-01-08 16:01:05 +0700

    Create delete_log.py

---
+++ delete_log.py
@@ -0,0 +1,27 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+# Định nghĩa DAG
+dag = DAG(
+    'delete_airflow_logs',
+    default_args={
+        'owner': 'airflow',
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='DAG to delete Airflow logs older than 1 day',
+    schedule_interval='@daily',  # Chạy hàng ngày
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+)
+
+# Câu lệnh Bash để xóa log cũ
+# Xóa các file log đã được tạo hơn 1 ngày trước
+delete_logs = BashOperator(
+    task_id='delete_old_logs',
+    bash_command="find /usr/local/airflow/logs -type f -mtime +1 -exec rm -f {} \;",
+    dag=dag,
+)
+
+delete_logs
